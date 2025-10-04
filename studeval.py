3843b85930ce71a5e851570aa270d48ae9f286f7
Joe <nathanielbandi636@gmail.com>
2025-10-04 09:03:35 +0530

    Update studeval.py

--- studeval.py
+++ studeval.py
@@ -1,28 +1,16 @@
-marks = []   
+# Program to accept marks and evaluate grade
 
-for i in range(1, 6):
-    m = int(input(f"Enter marks for subject {i}: "))
-    marks.append(m)
+marks = int(input("Enter your marks (0-100): "))
 
-
-total = sum(marks)
-average = total / 5
-
-
-if average >= 90:
+if marks >= 90:
     grade = "A"
-elif average >= 75:
+elif marks >= 75:
     grade = "B"
-elif average >= 60:
+elif marks >= 60:
     grade = "C"
-elif average >= 40:
+elif marks >= 40:
     grade = "D"
 else:
     grade = "Fail"
 
-# Output
-print("\n--- Result ---")
-print("Marks:", marks)
-print("Total:", total)
-print("Average:", average)
-print("Grade:", grade)
+print(f"Your grade is: {grade}")
